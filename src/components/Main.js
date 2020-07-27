9a2d0a4021f0f9525e9120e99780211bfdf8be8d
Manpreet Kaur <kaurman2305@gmail.com>
2020-07-27 18:24:54 +0530

    Initial Commit

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddIcon from '@material-ui/icons/Add';
+
+export const Main = () => {
+    return (
+        <div className="main-container">
+            <div className="utility">
+                <input type="text" placeholder="Search..."></input>
+                <span className="create">CREATE NEW</span>
+            </div>
+        </div>
+    )
+}
